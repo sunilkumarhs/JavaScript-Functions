26ac7705a661681de30c6df101165bd4130cb165
sunilkumarhs <sunilkumarhs974117@gmail.com>
2023-06-07 22:44:17 +0530

    bonus challnege

--- script.js
+++ script.js
@@ -246,3 +246,7 @@ const poll = {
 document
   .querySelector('.poll')
   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
